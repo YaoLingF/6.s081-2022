ce0837516c67acdcc472c75eb27f47ec96aa61a1
“YaoLingF” <1183887021@qq.com>
2024-06-17 15:10:50 +0800

    sleep

---
+++ user/sleep.c
@@ -0,0 +1,16 @@
+#include "../kernel/types.h"
+#include "user.h"
+int main(int argc,char *argv[])
+{
+    if(argc != 2)
+    {
+        fprintf(2, "Usage: sleep [ticks num]\n");
+    }
+    else
+    {
+        int k = atoi(argv[1]);
+        sleep(k);
+    }
+    exit(0);
+
+}
