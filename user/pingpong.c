a8fbf4949fe655770051f3cb7e8cad5f969dc7f1
“YaoLingF” <1183887021@qq.com>
2024-06-18 19:38:20 +0800

    pingpong

---
+++ user/pingpong.c
@@ -0,0 +1,31 @@
+#include "../kernel/types.h"
+#include "user.h"
+
+int main()
+{
+    int p[2];
+    pipe(p);
+    if(fork() == 0)//child
+    {
+        char buffer[1];
+        read(p[0],buffer,1);
+
+        write(p[1],buffer,1);
+
+        fprintf(1,"%d: received ping\n",getpid());
+    }
+    else//parent
+    {
+        char buffer[] = {'a'};
+        
+        write(p[1],buffer,1);
+
+        wait(0);
+
+        read(p[0],buffer,1);
+
+        fprintf(1,"%d: received pong\n",getpid());
+
+    }
+    exit(0);
+}
